a6e6809d19f456c66da7b2a26f7f6c6835a91e00
HEE <HEE@DESKTOP-TC6JPOG>
2023-11-11 11:25:28 +0700

    initital commit

---
+++ src/main/java/com/mycompany/utskelompokpbo/model.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.utskelompokpbo;
+
+/**
+ *
+ * @author HEE
+ */
+public class model {
+
+    String NomorNota, NamaPelanggan;
+    double TotalPanjang, Harga;
+    int Member;
+
+    public String getNomorNota() {
+        return NomorNota;
+    }
+
+    public void setNomorNota(String NomorNota) {
+        this.NomorNota = NomorNota;
+    }
+
+    public String getNamaPelanggan() {
+        return NamaPelanggan;
+    }
+
+    public void setNamaPelanggan(String NamaPelanggan) {
+        this.NamaPelanggan = NamaPelanggan;
+    }
+
+    public double getTotalPanjang() {
+        return TotalPanjang;
+    }
+
+    public void setTotalPanjang(double TotalPanjang) {
+        this.TotalPanjang = TotalPanjang;
+    }
+
+    public double getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(double Harga) {
+        this.Harga = Harga;
+    }
+
+    public int getMember() {
+        return Member;
+    }
+
+    public void setMember(int Member) {
+        this.Member = Member;
+    }
+
+
+}
